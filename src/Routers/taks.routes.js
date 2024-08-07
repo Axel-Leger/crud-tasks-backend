15df7bffe75ce22cfc1c20546f42800f2c1f8f59
Axel-Leger <axelleger2@gmail.com>
2024-08-07 12:45:31 -0300

    se termino el trabajo

--- src/Routers/taks.routes.js
+++ src/Routers/taks.routes.js
@@ -1,8 +1,19 @@
 
-const {traerTareas} = require("../controllers/controladores")
+const {traerTareas, traerTareasID,crearTaks,ModificarTarea,eliminarTarea} = require("../controllers/controladores")
 const router = require("express").Router();
 
 //obtener todas las tareas
 router.get("/task", traerTareas);
+//obtener por id 
+router.get("/task/:id", traerTareasID);
+//crear tarea
+router.post("/task", crearTaks);
+//modificar tarea
+router.put("/task/:id", ModificarTarea);
+//eliminar tarea
+router.delete("/task/:id", eliminarTarea);
+
+
+
 
 module.exports= router;
--- src/controllers/controladores.js
+++ src/controllers/controladores.js
@@ -17,13 +17,15 @@ crtl.traerTareas = async (req,res)=>{
         res.status(500).json({msg:"error en el servidor"})
     }
 }
+
+
+//crear tareas
 crtl.crearTaks = async ( req,res)=>{
     try{
-        const { title}= req.body;
-        const conexion = await conectDB();
+        const {title,description,isComplete}= req.body;
+        const conexion = await bd();
         await conexion.query('INSERT INTO  tasks ( title, description, isComplete) VALUES (?,?,?)',[
-            title
-        ]) ;
+            title,description,isComplete]) ;
 
             res.json({
                 msg:'tarea creada'
@@ -34,5 +36,67 @@ crtl.crearTaks = async ( req,res)=>{
     
 }
 
+//obtener por id 
+
+crtl.traerTareasID = async (req,res)=>{
+    try {
+        const coneccion = await bd();
+        const {id} = req.params
+
+        const [result]= await coneccion.query("SELECT * FROM tasks WHERE idTarea = ?", [id])
+            return  res.json(result)
+
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({msg:"error en el servidor"})
+    }
+}
+
+//modificar tarea
+crtl.ModificarTarea = async (req,res)=>{
+    
+    const coneccion = await bd();
+    const {id} = req.params
+    const {title,description,isComplete} = req.body
+    try {
+        const [encontrarTarea] = await coneccion.query("SELECT * FROM tasks WHERE idTarea = ?", [id])
+        if(encontrarTarea.length === 0){
+            console.log("tarea no encontrada");
+        }else{
+            const [result]= await coneccion.query("UPDATE tasks SET title= ?,description= ?,isComplete= ? WHERE idTarea = ?", [title,description,isComplete,id])
+            if(!result.ok){
+                return  res.status(200).json({msg:"se modifico con exito la tarea"});
+            }else{
+                return res.status(404).json({msg:'ocurrio un error al editar la tarea '})
+            }
+            
+        }
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({msg:"error en el servidor"})
+    }
+}
+
+
+
+//eliminar tarea
+crtl.eliminarTarea = async (req,res)=>{
+    
+    const coneccion = await bd();
+        const {id} = req.params
+    try {
+        const [resultado]= await coneccion.query('DELETE FROM tasks WHERE idTarea = ?',[id]);
+        if(!resultado.ok){
+            res.status(200).json({msg:'tarea eliminada correctamente'});
+        }
+
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({msg:"error en el servidor"})
+    }
+}
 
 module.exports = crtl
